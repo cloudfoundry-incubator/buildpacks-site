091a319bf20e9d02fed5a8f56397718c08c790ad
Dave Goddard <dave@goddard.id.au>
2017-04-26 22:03:27 -0400

    vue-resource

--- src/components/Buildpack.vue
+++ src/components/Buildpack.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
     <h2>{{ buildpack.name }}</h2>
+
     <a :href="github" target="_blank">Github</a>
+    <ul>
+      <li v-for="release in buildpack.releases">
+        {{ release.tag_name }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -12,9 +18,14 @@ export default {
   name: 'buildpack',
   props: ['id'],
   data: function () {
-    var id = this.id
-    var buildpack = buildpacks.find(function (b) { return b.id === id })
-    return { buildpack }
+    return { buildpack: { releases: [] } }
+  },
+  beforeRouteEnter (to, from, next) {
+    next(vm => vm.setBuildpack(to.params.id))
+  },
+  beforeRouteUpdate (to, from, next) {
+    this.setBuildpack(to.params.id)
+    next()
   },
   computed: {
     link: function () {
@@ -22,6 +33,22 @@ export default {
     },
     github: function () {
       return `https://github.com/${this.buildpack.repo}`
+    }
+  },
+  methods: {
+    setBuildpack (id) {
+      var buildpack = Object.assign({}, buildpacks.find(function (b) { return b.id === id }))
+      buildpack.releases = []
+      this.buildpack = buildpack
+      this.fetchData()
+    },
+    fetchData () {
+      this.$http.get(`https://api.github.com/repos/${this.buildpack.repo}/releases`).then(resp => {
+        this.buildpack.releases = resp.data
+      }, err => {
+        alert(err)
+        console.log(err)
+      })
     }
   }
 }
