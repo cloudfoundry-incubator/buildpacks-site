30a59ac4c0f3c934aab31752b1a56d2b4f9bb9ea
Dave Goddard <dave@goddard.id.au>
2017-05-12 11:30:05 -0400

    WIP; use testcafe instead of nightwatch

---
+++ test/e2e/simple.test.js
@@ -0,0 +1,14 @@
+import { Selector } from 'testcafe'
+
+fixture('Simple flow')
+  .page('http://localhost:8080')
+
+test('Flow', async t => {
+  await t
+    .resizeWindow(600, 800)
+    .expect(Selector('.name').innerText).eql('Ruby Buildpack')
+    .click('a.latestversion')
+    .expect(Selector('.buildpackdetail h1').innerText).eql('Ruby Buildpack v1.6.38')
+    .click(Selector('a').withText('v1.6.33'))
+    .expect(Selector('.buildpackdetail h1').innerText).eql('Ruby Buildpack v1.6.33')
+})
