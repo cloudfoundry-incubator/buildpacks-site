2466ea010bfe97c51fca46155c36f78d15eeb897
Dave Goddard <dave@goddard.id.au>
2017-05-18 13:30:40 -0400

    Show 4 digit year
    Signed-off-by: Keaty Gross <kgross@pivotal.io>

--- src/filters.js
+++ src/filters.js
@@ -2,5 +2,5 @@ import Vue from 'vue'
 import moment from 'moment'
 
 Vue.filter('humanReadableDate', function (date) {
-  return moment(date).format('MMM Do YY')
+  return moment(date).format('MMM Do YYYY')
 })
