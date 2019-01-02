db4ef1e6a64cb582622ae9049891f0018a6d3259
Tyler Phelan <tphelan@pivotal.io>
2019-01-02 15:32:58 -0500

    Update testcafe to non-vulnerable version
    - Update e2e test with updated site
    
    [#162241459]
    
    Co-authored-by: Danny Joyce <djoyce@pivotal.io>

--- test/e2e/simple.test.js
+++ test/e2e/simple.test.js
@@ -6,11 +6,10 @@ fixture('Simple flow')
 test('Flow', async t => {
   await t
     .resizeWindow(600, 800)
-    .expect(Selector('.name').innerText).eql('Ruby Buildpack')
+    .expect(Selector('.name').innerText).eql('Binary Buildpack')
     .click('a.latestversion')
-    .expect(Selector('.buildpackdetail h1').innerText).contains('Ruby Buildpack v1.')
+    .expect(Selector('.buildpackdetail h1').innerText).contains('Binary Buildpack v')
     .click(Selector('button').withText('Show Outdated Versions'))
-    .click(Selector('a').withText('v1.6.33'))
-    .expect(Selector('.buildpackdetail h1').innerText).eql('Ruby Buildpack v1.6.33')
+    .click(Selector('a').withText('v1.0.26'))
+    .expect(Selector('.buildpackdetail h1').innerText).eql('Binary Buildpack v1.0.26')
 })
-
